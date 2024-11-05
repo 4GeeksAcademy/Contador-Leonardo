d4642c11d6e055214a96cb422e1866bfc9983fd1
Leonardo Medina <leonardomedina1250@gmail.com>
2024-11-05 09:18:54 +0000

    Commit con estilos

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -3,22 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 function SecondsCounter({ seconds }) {
-  const minutes = Math.floor(seconds / 60); // minutos
-  const secs = Math.floor(seconds % 60); // segundos
-  const secsTens = Math.floor(secs / 10); // Dígito de las decenas
-  const secsUnits = secs % 10; // Dígito de las unidades
+  const minutos = Math.floor(seconds / 60); // Minutos
+  const minutosDecena = Math.floor(minutos / 10); // Decenas de minutos
+  const minutosUnidades = minutos % 10; // Unidades de minutos
+  const segundos = Math.floor(seconds % 60); // Segundos
+  const segDecena = Math.floor(segundos / 10); // Decenas de segundos
+  const segUnidades = segundos % 10; // Unidades de segundos
 
   return (
-    <div>
-      <h1><FontAwesomeIcon icon={faClock} /> Contador de tiempo</h1>
-      <p>
-        <span className="minutes">{`${minutes}`.padStart(2, '0')}</span>
-        :
-        <span className="seconds">
-          <span className="secsTens">{`${secsTens}`}</span>
-          <span className="secsUnits">{`${secsUnits}`}</span>
-        </span>
-      </p>
+    <div  className="container">
+      <div className="row">
+        <div id="contador" className="col-12 col-md-8 mx-auto mt-5 text-center">
+          <h1> Contador de tiempo</h1>
+          <p>
+            <FontAwesomeIcon icon={faClock} />
+            <span className="minutes">
+              <span className="minsTens Digito">{`${minutosDecena}`}</span>
+              <span className="minsUnits Digito">{`${minutosUnidades}`}</span>
+            </span>
+            :
+            <span className="seconds">
+              <span className="secsTens Digito">{`${segDecena}`}</span>
+              <span className="secsUnits Digito">{`${segUnidades}`}</span>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
